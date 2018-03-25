de64c104ceebb465554a897257534b7fe0e75557
bmlegge <36506641+bmlegge@users.noreply.github.com>
2018-03-25 19:20:13 -0400

    Add files via upload
    Python 4 Running Total

---
+++ P4HW2_RunningTotal_Legge.py
@@ -0,0 +1,14 @@
+#CTI-110
+#P4HW2: Running Total
+#Bradley Legge
+#3/25/2018
+
+num = 0
+total = 0
+
+while num >= 0:
+    print("Enter a number: ")
+    num = int(input())
+    total = total + num
+
+print("Total: ", total)
